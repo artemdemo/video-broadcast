c153ceb9438bcd3218e64b08166ba641b5780fec
Artem Demo <artemdemo@gmail.com>
2020-04-14 10:54:12 +0300

    updated packages

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ echo ""
 # Start multiple processes
 # and be able to kill them all with `Ctrl + c`
 # @source: https://stackoverflow.com/a/52033580
-(trap 'kill 0' SIGINT; cd ./app; npm start & cd ../; npm start)
+(trap 'kill 0' SIGINT; cd ./app; npm start & cd ../server; npm start)
